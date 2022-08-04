710cd5db31b336cfa158fe38b9b580ae9884a69a
Daniel Marques <daniel.star8@outlook.com>
2022-08-04 15:27:12 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,63 +1,64 @@
 const controle = document.querySelectorAll("[data-controle]");
 const estatisticas = document.querySelectorAll("[data-estatistica]")
 const pecas = {
-   "bracos": {
-       "forca": 29,
-       "poder": 35,
-       "energia": -21,
-       "velocidade": -5
-   },
+  "bracos": {
+    "forca": 29,
+    "poder": 35,
+    "energia": -21,
+    "velocidade": -5
+  },
 
-   "blindagem": {
-       "forca": 41,
-       "poder": 20,
-       "energia": 0,
-       "velocidade": -20
-   },
-   "nucleos":{
-       "forca": 0,
-       "poder": 7,
-       "energia": 48,
-       "velocidade": -24
-   },
-   "pernas":{
-       "forca": 27,
-       "poder": 21,
-       "energia": -32,
-       "velocidade": 42
-   },
-   "foguetes": {
-       "forca": 0,
-       "poder": 28,
-       "energia": 0,
-       "velocidade": -2
-   }
+  "blindagem": {
+    "forca": 41,
+    "poder": 20,
+    "energia": 0,
+    "velocidade": -20
+  },
+
+  "nucleos":{
+    "forca": 0,
+    "poder": 7,
+    "energia": 48,
+    "velocidade": -24
+  },
+  "pernas":{
+    "forca": 27,
+    "poder": 21,
+    "energia": -32,
+    "velocidade": 42
+  },
+  "foguetes": {
+    "forca": 0,
+    "poder": 28,
+    "energia": 0,
+    "velocidade": -2
+  }
 }
 
 controle.forEach((element)=>{
-   element.addEventListener('click', (event)=>{
-      manipulaDados(event.target.dataset.controle, event.target.parentNode)
-      atualizaEstatistica(event.target.dataset.peca);
-   })
+  element.addEventListener('click', (event)=>{
+    manipulaDados(event.target.dataset.controle, event.target.parentNode)
+    atualizaEstatistica(event.target.dataset.peca);
+  })
 })
 
 function manipulaDados(operacao, controle) {
-   const peca = controle.querySelector("[data-contador]")
+  const peca = controle.querySelector("[data-contador]")
 
-   if(operacao === "-") { //subtrair 1
-      peca.value = parseInt(peca.value) - 1
-   } else { //somar 1
-      peca.value = parseInt(peca.value) + 1
-   }
+  if(operacao === "-") { //subtrair 1
+  peca.value = parseInt(peca.value) - 1
+  }else { //somar 1
+  peca.value = parseInt(peca.value) + 1
+  }
 }
 
 function atualizaEstatistica(peca) {
-   estatisticas.forEach((element) => {
-      element.textContent = parseInt(element.textContent) + pecas[peca][element.dataset.estatistica]
-   })
+  estatisticas.forEach((element) => {
+    element.textContent = parseInt(element.textContent) + pecas[peca][element.dataset.estatistica]
+  })
 }
 
 function trocaImagem(cor) {
-   document.querySelector(".robo").src="img/Robotron 2000 - " + cor + ".png";
+  document.querySelector(".robo").src="img/Robotron 2000 - " + cor + ".png";
 }
 
